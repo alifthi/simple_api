bb96691713048c10c3e7f161f5b2ee564d82bbf3
Alifthi <alifathi8008@gmail.com>
2024-02-20 16:49:13 +0330

    simple_api: sending language to the API.

--- main.py
+++ main.py
@@ -6,8 +6,10 @@ app = Flask(__name__)
 
 device = "cuda" # the device to load the model onto
 
-model = AutoModelForCausalLM.from_pretrained("mistralai/Mixtral-8x7B-v0.1")
-tokenizer = AutoTokenizer.from_pretrained("mistralai/Mixtral-8x7B-v0.1")                                 
+model = AutoModelForCausalLM.from_pretrained(
+    "Qwen/Qwen1.5-7B-Chat",
+    device_map="auto")
+tokenizer = AutoTokenizer.from_pretrained("Qwen/Qwen1.5-7B-Chat")                                       
 
   
 @app.route("/", methods=['POST'])
@@ -16,12 +18,11 @@ def receive_json():
         return jsonify({"error": "No JSON data found in request"}), 400
     try:
         messages = request.get_json()
-        
     except Exception as e:
         return jsonify({"error": "Invalid JSON format"}), 400
-    
+    prompts=messages['message']
     text = tokenizer.apply_chat_template(
-            [messages],
+            [prompts],
         tokenize=False,
         add_generation_prompt=True)
     model_inputs = tokenizer([text], return_tensors="pt").to(device)
@@ -34,8 +35,9 @@ def receive_json():
     output_ids[len(input_ids):] for input_ids, output_ids in zip(model_inputs.input_ids, generated_ids)]
 
     response = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
-
-    return response          
+    is_first_message=messages['is_first_message']
+    language=messages['lang']
+    return jsonify({'response':response})          
     
 if __name__ == '__main__': 
    app.run(debug = True) 
--- send.py
+++ send.py
@@ -4,13 +4,14 @@ import json
 data = {"name": "Alice", "age": 30}
 
 headers = {"Content-Type": "application/json"}
-prompt="Do NER on the following text without any explenation: \n set alarm for 9:30"
-messages = {"role": "user", "content": prompt}
+prompt="Do NER on the following text without more explenation: \n set alarm for 9:30"
+messages = {'lang':'persian','is_first_message':True,'message':{"role": "user", "content": prompt}}
 messages=json.dumps(messages)
 response = requests.post("http://localhost:5000/", data=messages, headers=headers)
 
 if response.status_code == 200:
-    response=response.text
+    response=response.json()
+    response=response['response']
     print("JSON file sent successfully!",response)
 else:
     print("Error sending file:", response.content.decode())
