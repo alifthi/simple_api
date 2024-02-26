9cc4759417d379b374ce25cdf301f575ca91b18c
Alifthi <alifathi8008@gmail.com>
2024-02-26 12:14:20 +0330

    simple_api: prompt file is created to manange the prompts

--- send.py
+++ send.py
@@ -1,18 +1,17 @@
 import requests
 import json
-
-data = {"name": "Alice", "age": 30}
-
+import time 
 headers = {"Content-Type": "application/json"}
-prompt="Do NER on the following text without more explenation: \n set alarm for 9:30"
-prompt='how can i cook gorme sabzi?'
-messages = {'lang':'persian','is_first_message':True,'message':{"role": "user", "content": prompt}}
+prompt="چطور میتونم سرعت اجرا یه برنامه رو افزایش بدم؟"
+messages = {'lang':'persian','is_first_message':True,'message':
+			{"role": "user", "content":prompt}}
 messages=json.dumps(messages)
+a=time.time()
 response = requests.post("http://79.127.125.211:44580", data=messages, headers=headers)
 
 if response.status_code == 200:
     response=response.json()
     response=response['response']
-    print("JSON file sent successfully!",response)
+    print(f'response recived in {time.time()-a} s',response)
 else:
     print("Error sending file:", response.content.decode())
