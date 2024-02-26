fc4d7218ab6a059ebf5bd7105db44ca9ba8d2c66
Alifthi <alifathi8008@gmail.com>
2024-02-26 17:39:42 +0330

    simple_api: change the name of prompt file to utils

--- send.py
+++ send.py
@@ -3,8 +3,8 @@ import json
 import time 
 headers = {"Content-Type": "application/jsnon"}
 # prompt="Do NER on the following sentence: \nNote: **just say the entities in  [{\'entities\': Names, \'types\': types}] structure** \n set alarm for 8 AM"
-prompt="Play a music"
-prompt=f"Extract the all entities of the following sentence **say the entities like a json\n {prompt}"
+prompt="call Ali"
+prompt=f"Extract the entities of the following sentence **say the entities like a json\n {prompt}"
 messages = {'lang':'persian','is_first_message':True,'message':
 			{"role": "user", "content":prompt}}
 messages=json.dumps(messages)
--- utils.py
+++ utils.py
@@ -4,5 +4,8 @@ class prompt:
     def fistMessage(self,message): 
         return f' <|im_start|> is this massage wants some thing about android phone? just say yes or no:\n{message}<|im_end|> <|im_start|>assistant' 
     def extractNER(self,response):
-        prompt=f"<|im_start|> Extract the all entities of the following sentence **say the entities like a json\n {response} <|im_end|> <|im_start|>assistant"
+        prompt=f"<|im_start|> Extract the entities of the following sentence **say the entities like a json\n {response} <|im_end|> <|im_start|>assistant"
         return prompt
+class model:
+    def __init__(self) -> None:
+        pass
